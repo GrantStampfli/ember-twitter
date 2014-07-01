5d7ac712633acad7973b2cf31f6cc48ca38e67f3
Grant Stampfli <mail@gstampfli.com>
2014-07-01 15:12:09 -0700

    removed example

--- admit-one-master/lib/adapter/load.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = function(name, opts) {
-  try { return require('admit-one-' + name)(opts); }
-  catch (e) {
-    throw new Error('Could not find admit-one adapter ' + name + '.');
-  }
-};
