ce128427de123a9162e85920575b0f47cb81cb2a
Grant Stampfli <mail@gstampfli.com>
2014-07-02 11:23:57 -0700

    set up test env

--- test/app/test.js
+++ test/app/test.js
@@ -1,6 +1,11 @@
-/* global describe, it */
-
+/* global describe, it, App, DS */
 'use strict';
+
+var fixture = require('./fixtures/example.json');
+
+App.ApplicationAdapter = DS.LSAdapter.extend({
+	namespace: 'ember-twitter'
+});
 
 describe('app', function() {
   it('has one passing test', function() {});
