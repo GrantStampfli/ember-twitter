95a1440b0bada691f69990f6d7881f1fabab42bb
Tian Song <tian.y.song@gmail.com>
2014-07-03 10:32:28 -0700

    using single quotes in js.

--- server/application.js
+++ server/application.js
@@ -63,9 +63,9 @@ api.post('/sessions', admit.authenticate, function(req, res) {
 
 api.get('/barks', function(req, res){
   res.json({ barks:[
-  { id:1, author: 0, date: "2014-07-01", "content": "this is some content from Ariel"},
-  { id:2, author: 1, date: "2014-06-05", "content": "this is some content from Tian"},
-  { id:3, author: 2, date: "2014-05-05", "content": "this is some content from Grant"}
+  { id:1, author: 0, date: '2014-07-01', content: 'this is some content from Ariel'},
+  { id:2, author: 1, date: '2014-06-05', content: 'this is some content from Tian'},
+  { id:3, author: 2, date: '2014-05-05', content: 'this is some content from Grant'}
 ]
 });
 });
